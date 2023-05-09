adfded59ceea367dfbd3c1dd687eb2ad0c421280
ereburg <gimmethenick@gmail.com>
2023-05-09 20:29:43 +0300

    add register form

--- frontend/src/app/consts.ts
+++ frontend/src/app/consts.ts
@@ -1,7 +1,15 @@
+import { HexString } from '@polkadot/util/types'
+
 export const LOCAL_STORAGE = {
   ACCOUNT: 'account',
 }
 
 export const ENV = {
   NODE: import.meta.env.VITE_NODE_ADDRESS,
+}
+
+export const initialRegister = {
+  programId: '' as HexString,
+  programId2: '' as HexString,
+  currentStep: 1,
 }
